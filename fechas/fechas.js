12e968ccd38e01e50d6feed4cb5ea6daf7613ad1
dbsalazarr <dbsalazarr@gmail.com>
2023-10-25 12:55:24 -0500

    Lógica del reto de las fechas terminado
     Se esta iniciando con la parte gráfica

--- fechas/fechas.js
+++ fechas/fechas.js
@@ -1,9 +1,29 @@
-
-
 const year = 2022
 const holidays = ['01/06', '04/01', '12/25']
-console.log(year +"/"+ holidays[0])
-function diaSemana( dia ){
+const weekDays = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"]
+
+function generarFechas(date_list){
+    let new_dates = []
+    date_list.forEach( (date) => {
+        new_dates.push(year+"/"+date)
+    });
+    return new_dates
+}
+
+// console.log( generarFechas(holidays) )
+
+function calcularHorasCompensacion(){
+    let new_dates = generarFechas(holidays)
+    let cantidadDiasLaborales = 0
+    new_dates.forEach( (date) => {
+        cantidadDiasLaborales = esDiaLaboral( obtenerDiaSemana(date) ) ?  cantidadDiasLaborales += 1 : cantidadDiasLaborales += 0
+    })
+    return cantidadDiasLaborales*2
+}
+
+console.log("La cantidad de horas a recuperar es: ", calcularHorasCompensacion())
+
+function esDiaLaboral( dia ){
     dia = dia.toLowerCase();
     switch (dia){
         case "lunes":
@@ -20,7 +40,10 @@ function diaSemana( dia ){
     }
 }
 
-function obtenerDiaSemana(){
-    const dia = new Date();
-
+function obtenerDiaSemana( fecha ){
+    const nuevaFecha = new Date(fecha);
+    let dia = weekDays[ nuevaFecha.getDay() ]
+    return dia
 }
+
+console.log( obtenerDiaSemana("2022/04/1") )
