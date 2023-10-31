52a12488b91397a16c2355b29b81a7b8064d940e
dbsalazarr <dbsalazarr@gmail.com>
2023-10-31 12:02:10 -0500

    Eventos en Javascript

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () =>{
     console.log("El sitio web, esta listo!")
 })
+
+
+// seleccionando elemento
+let button = document.getElementById("btnEnviar")
+
+// agregando el evento clikc
+
+button.addEventListener('click', (e) => {
+	console.log(e)
+})
